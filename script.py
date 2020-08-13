f1d1551472614714d895b2f2914b38da035ebbc8
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-12 23:54:17 -0700

    created logic to filter out the files and directories

--- script.py
+++ script.py
@@ -15,4 +15,18 @@ parser.add_argument(
 args = parser.parse_args()
 path = args.path
 
-print(f"Clearning up directory {path}")
+# gets the list of all the files and folders inside of that directory
+content = os.listdir(path)
+
+# creates a relative path from the path to the file and the document name
+path_content = [os.path.join(path, doc) for doc in content]
+
+# filters out only the files and assigns it to the docs variable
+docs = [doc for doc in path_content if os.path.isfile(doc)]
+folders = [folder for folder in path_content if os.path.isdir(doc)]
+
+moved = 0
+created_folder = []
+
+
+print(f"Clearning up directory {docs}")
